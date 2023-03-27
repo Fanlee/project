62344c120999ca46353e54d12538733aa5031e84
fanlee <anyueleo@163.com>
2023-03-27 22:38:41 +0800

    ✨ feat: 添加下载成功提示

--- cli/lib/core/download.js
+++ cli/lib/core/download.js
@@ -5,10 +5,13 @@ const chalk = require('chalk')
 function downloadRepo(url, project) {
   const spinner = ora().start()
   spinner.text = '代码下载中，请稍等...'
-  download(`direct:${url}`, project, {clone:true},  err => {
-    if (!err) { 
+  download(`direct:${url}`, project, { clone: true }, (err) => {
+    if (!err) {
       spinner.succeed('代码下载成功')
-      console.log(chalk.green.bold('Done!'))
+      console.log(chalk.green.bold('Done!'), 'you run:')
+      console.log(`cd ${project}`)
+      console.log('npm install')
+      console.log('npm run dev')
     } else {
       spinner.fail('代码下载失败')
     }
