afb3138f605dde0d3ce72ccb18905db75fc23194
fanlee <anyueleo@163.com>
2023-03-29 22:55:13 +0800

    ✨ feat: 基础模块搭建

--- express-project/app.js
+++ express-project/app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
+const router = require('./router')
 const app = express()
 
-const PORT = process.env.PORT || 3000
-
+// 解析客户端请求
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+// 日志中间件
+app.use(morgan('dev'))
 // 挂载路由
-app.use('/api', router)
+app.use('/api/v1', router)
 
-// 挂载统一处理服务端错误中间件
-app.use(errorHandler())
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
